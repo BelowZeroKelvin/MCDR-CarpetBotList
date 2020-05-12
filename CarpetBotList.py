4f5dbcb7f66b3120687794af54bc683406415fa2
BelowZeroKelvin <63833182+BelowZeroKelvin@users.noreply.github.com>
2020-05-12 11:34:53 +0800

    添加快捷下线

--- CarpetBotList.py
+++ CarpetBotList.py
@@ -1,5 +1,6 @@
 # coding : utf-8
 import re
+from utils.rtext import RTextList,RText,RAction,RColor
 
 bot_list = []
 
@@ -38,13 +39,18 @@ def list_bot(server):
 
 
 def msg_list_bot(server):
-    msg = '§7[假人列表]\n'
+    if not len(bot_list):
+        return '§7服务器还没有假人'
     new_list = list_bot(server)
-    print(new_list)
+    msg=RTextList(RText("[假人列表]",color=RColor.gray))
     for bot in new_list:
-        print(bot)
-        msg += bot[0] + ' 位于[' + worlds[bot[1][0]] + '§r] (' + ','.join(
-            [str(int(i)) for i in bot[1][1]]) + ')\n'
+            msg+=RTextList(
+                RText('\n[x] ',color=RColor.red)
+                    .h('下线该假人')
+                    .c(RAction.run_command,'/player '+bot[0]+' kill'),
+                RText(bot[0])
+                    .h(RText(' [' + worlds[bot[1][0]] + '§r] \n(' + ','.join([str(int(i)) for i in bot[1][1]]) + ') '))
+            )
     return msg
 
 
@@ -57,7 +63,7 @@ def on_info(server, info):
     elif info.is_player:
         cmd = info.content.split()
         if len(cmd) == 1 and cmd[0] == '!!botlist':
-            say_lines(server, msg_list_bot(server))
+            server.say(msg_list_bot(server))
 
 
 def on_player_left(server, player):
@@ -68,7 +74,7 @@ def on_player_left(server, player):
 
 def on_load(server, old_module):
     global bot_list
-    if old_module and server.is_server_startup():
+    if old_module:
         bot_list = old_module.bot_list
 
 def on_server_startup(server):
